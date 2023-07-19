0cf84162b2920b57d4cfbf94df353af4bf0f8312
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2023-07-19 13:05:49 +0200

    styles

--- uikit/Header/Header.tsx
+++ uikit/Header/Header.tsx
@@ -32,42 +32,40 @@ const Header = () => {
 
   return (
     <header className={classNames(font.className, styles.header)}>
-      <div className="container">
-        <div className={styles.headerContent}>
-          <div className={styles.logoContainer}>
-            <Link href="/"><Image className={styles.logo} src="/images/Logo.png" width={108} height={157} alt="Tranped s.r.o logo" /></Link>
-          </div>
-          <nav className={styles.nav}>
-            <Link href="/">Головна</Link>
-            <Link href="/products">Продукти</Link>
-            <Link href="/investors">Звʼязок з інвесторами</Link>
-            <Link href="/logystics">Логістика</Link>
-            <Link href="/quality">Якість</Link>
-            <Link href="/contacts">Контакти</Link>
-          </nav>
-          <div className={styles.rightActions}>
-            <div className={styles.link}>
-              <div className={styles.top}>{i18n.language}<IoIosArrowDown size={16} /></div>
-              <div className={styles.bot}>
-                <div className={styles.links}>
-                  {LANGS.filter((l) => l !== i18n.language).map((l) => (
-                    <Link key={l} className={styles.link} href={'/'} locale={l}>
-                      <Image
-                        src={getFlag(l)}
-                        width="20"
-                        height="14"
-                        alt={l}
-                      />
-                      {l}
-                    </Link>
-                  ))}
-                </div>
+      <div className={styles.headerContent}>
+        <div className={styles.logoContainer}>
+          <Link href="/"><Image className={styles.logo} src="/images/Logo.png" width={108} height={157} alt="Tranped s.r.o logo" /></Link>
+        </div>
+        <nav className={styles.nav}>
+          <Link href="/">Головна</Link>
+          <Link href="/products">Продукти</Link>
+          <Link href="/investors">Звʼязок з інвесторами</Link>
+          <Link href="/logystics">Логістика</Link>
+          <Link href="/quality">Якість</Link>
+          <Link href="/contacts">Контакти</Link>
+        </nav>
+        <div className={styles.rightActions}>
+          <div className={styles.link}>
+            <div className={styles.top}>{i18n.language}<IoIosArrowDown size={16} /></div>
+            <div className={styles.bot}>
+              <div className={styles.links}>
+                {LANGS.filter((l) => l !== i18n.language).map((l) => (
+                  <Link key={l} className={styles.link} href={'/'} locale={l}>
+                    <Image
+                      src={getFlag(l)}
+                      width="20"
+                      height="14"
+                      alt={l}
+                    />
+                    {l}
+                  </Link>
+                ))}
               </div>
             </div>
-            <button className={styles.burgerMenu}>
-              <IoMenuSharp size={30} />
-            </button>
           </div>
+          <button className={styles.burgerMenu}>
+            <IoMenuSharp size={30} />
+          </button>
         </div>
       </div>
     </header>
