8195b9a847f29528f965c03d628b0926567e3c83
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2023-07-21 14:03:41 +0200

    Add mobile footer

--- uikit/Footer/Footer.tsx
+++ uikit/Footer/Footer.tsx
@@ -10,22 +10,26 @@ const font = Montserrat({ subsets: ['latin'] });
 export default function Footer() {
   return (
     <footer className={`${styles.footer} ${font.className}`}>
-      <Image className={styles.logo} src="/images/Logo.png" width={137} height={200} alt="Tranped s.r.o logo" />
-      <div className={styles.pages}>
-        <Link href="/investors">Інвестування</Link>
-        <Link href="/contacts">Контакти</Link>
-        <Link href="/logystics">Логістика</Link>
-        <Link href="/products">Продукти</Link>
+      <div className={styles.sub}>
+        <Image className={styles.logo} src="/images/Logo.png" width={137} height={200} alt="Tranped s.r.o logo" />
+        <div className={styles.pages}>
+          <Link href="/investors">Інвестування</Link>
+          <Link href="/contacts">Контакти</Link>
+          <Link href="/logystics">Логістика</Link>
+          <Link href="/products">Продукти</Link>
+        </div>
       </div>
-      <div className={styles.phones}>
-        <div>Телефон склад</div>
-        <a href="tel:+421 56 67 23 940">+421 56 67 23 940</a>
-      </div>
-      <div className={styles.phones}>
-        <div>Телефон офіс</div>
-        <a href="tel:+421 917 336 901">+421 917 336 901</a>
-        <a href="tel:+421 907 934 592">+421 907 934 592</a>
-        <a href="tel:+421 907 672 239">+421 907 672 239</a>
+      <div className={styles.sub}>
+        <div className={styles.phones}>
+          <div>Телефон склад</div>
+          <a href="tel:+421 56 67 23 940">+421 56 67 23 940</a>
+        </div>
+        <div className={styles.phones}>
+          <div>Телефон офіс</div>
+          <a href="tel:+421 917 336 901">+421 917 336 901</a>
+          <a href="tel:+421 907 934 592">+421 907 934 592</a>
+          <a href="tel:+421 907 672 239">+421 907 672 239</a>
+        </div>
       </div>
       <div className={styles.socials}>
         <a><FaFacebook size={50} /></a>
