ad3fbd23a0e89695246b0c478fe471a673b44f56
bohdan.radoveniuk <53336828+radoveniuk@users.noreply.github.com>
2023-08-04 18:29:11 +0200

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'uk',
+    defaultLocale: 'sk',
     locales: ['uk', 'sk', 'en', 'hu'],
     // locales: ['uk', 'sk', 'en', 'de', 'it', 'hu', 'pl'],
   },
